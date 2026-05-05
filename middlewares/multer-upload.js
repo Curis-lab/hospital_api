0d782215637839ab3883b9104f5ae0bcf2b9d4eb
Boris <72118807+Curis-lab@users.noreply.github.com>
2026-05-05 22:09:59 +0630

    make multer as middleware

--- middlewares/multer-upload.js
+++ middlewares/multer-upload.js
@@ -1,38 +1,38 @@
 import multer from "multer";
 
 class MulterMiddleware {
-  imageUpload() {
-    const FIVE_MB = 5 * 1024 * 1024;
+	imageUpload() {
+		const FIVE_MB = 5 * 1024 * 1024;
 
-    const uploadImage = multer({
-      storage: multer.memoryStorage(),
-      limits: {
-        fileSize: FIVE_MB,
-      },
-      fileFilter: (req, file, cb) => {
-        if (file.mimetype.startsWith("image/")) {
-          cb(null, true);
-        } else {
-          cb(new Error("Only image files are allowed!"), false);
-        }
-      },
-    });
-    return uploadImage.single("image");
-  }
-  fileUpload() {
-    const savedPath = "uploads/";
-    const storage = multer.diskStorage({
-      destination: (req, file, cb) => {
-        cb(null, savedPath);
-      },
+		const uploadImage = multer({
+			storage: multer.memoryStorage(),
+			limits: {
+				fileSize: FIVE_MB,
+			},
+			fileFilter: (req, file, cb) => {
+				if (file.mimetype.startsWith("image/")) {
+					cb(null, true);
+				} else {
+					cb(new Error("Only image files are allowed!"), false);
+				}
+			},
+		});
+		return uploadImage.single("image");
+	}
+	fileUpload() {
+		const savedPath = "uploads/";
+		const storage = multer.diskStorage({
+			destination: (req, file, cb) => {
+				cb(null, savedPath);
+			},
 
-      filename: (req, file, cb) => {
-        cb(null, Date.now() + "-" + file.originalname);
-      },
-    });
-    const upload = multer({ storage });
-    return upload.single("file");
-  }
+			filename: (req, file, cb) => {
+				cb(null, Date.now() + "-" + file.originalname);
+			},
+		});
+		const upload = multer({ storage });
+		return upload.single("file");
+	}
 }
 
 const multerMiddleware = new MulterMiddleware();
