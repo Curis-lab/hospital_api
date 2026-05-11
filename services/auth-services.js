927385e817aeded7ab73e9f30e6236b1fc4b2214
Boris <72118807+Curis-lab@users.noreply.github.com>
2026-05-11 14:37:01 +0630

    add defensive for function

--- services/auth-services.js
+++ services/auth-services.js
@@ -2,22 +2,29 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
 async function decrpytPwd(pwd) {
-	const salted = await bcrypt.genSalt(10);
-	const hashed = await bcrypt.hash(pwd, salted);
-	return hashed;
+  if (!pwd) throw new Error("Password did not have in decryptPwd function.");
+  const password = pwd;
+  const salted = await bcrypt.genSalt(10);
+  const hashed = await bcrypt.hash(password, salted);
+  return hashed;
 }
 
 function tokenGenerate(data) {
-	const { _id, role } = data;
-	return jwt.sign({ id: _id, role }, process.env.JWT_SECRET, {
-		expiresIn: "15d",
-	});
+  const { _id, role } = data;
+  return jwt.sign({ id: _id, role }, process.env.JWT_SECRET, {
+    expiresIn: "15d",
+  });
 }
 
 async function isCorrectPassword(currentpwd, dbpwd) {
-	
-	const isMatch = await bcrypt.compare(currentpwd, dbpwd);
-	return isMatch;
+  if (dbpwd === "undefined") {
+    throw new Error("Database did not response password.");
+  }
+  if (currentpwd === "undefined") {
+    throw new Error("user did not input password.");
+  }
+  const isMatch = await bcrypt.compare(currentpwd, dbpwd);
+  return isMatch;
 }
 
 export { decrpytPwd, isCorrectPassword, tokenGenerate };
